f099e329053a734f891e51cd5ce1b1eda1595223
Yannick Djomo <67871275+franckya@users.noreply.github.com>
2022-05-07 23:39:03 -0700

    Create remove.py

---
+++ remove.py
@@ -0,0 +1,31 @@
+import json
+import requests
+import os
+
+#db_password= os.environ.get('mytoken')
+mytoken = 'ACCESS_TOKEN'
+apiurl = 'https://example.com/api/v4/'
+page = 0
+per_page = 100
+fetch = 1
+user_id = 'USER_ID_HERE'
+channel_id = 'CHANNEL_ID_HERE'
+base_url = 'https://example.com/api/v4/channels/'
+final_url = "https://example.com/api/v4/channels/CHANNEL_ID_HEREc/members/USER_ID_HERE"
+
+#final_url= base_url+'channel_id'+"/members"+'user_id'
+while(fetch):
+    r = requests.get(apiurl + '/channels?page=' + str(page) + '&per_page=' + str(per_page), headers={"Authorization": "Bearer " + mytoken})
+    #print(r.status_code)
+    data = r.json()
+    #print(data)
+    json_len = len(data)
+    page = page + 1
+    #print("Page " + str(page) + " Length: " + str(json_len))
+    for record in data:
+        #print(record['type'])
+        if 'P' in record["type"]:
+            r = requests.request("DELETE", final_url , headers={"Authorization": "Bearer " + mytoken});
+            print(r.status_code)
+            fetch = 0
+            break;
